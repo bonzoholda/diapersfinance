5249ce8a283fc236d888dd0381bec4a09c1aac8a
bonzoholda <bhumibeka@gmail.com>
2026-07-28 22:01:48 +0700

    Update Pool.js

--- src/components/Pool.js
+++ src/components/Pool.js
@@ -1,12 +1,16 @@
 import { Flex } from '@chakra-ui/react';
+
 const Pool = ({ children }) => {
   return (
     <Flex
       rounded={20}
-      bg="white"
+      bg="rgba(255, 255, 255, 0.15)"
+      backdropFilter="blur(12px)"
+      border="1px solid rgba(255, 255, 255, 0.25)"
+      boxShadow="0 8px 32px 0 rgba(0, 0, 0, 0.37)"
       px={6}
       py={10}
-      color="brand.purple"
+      color="white"
       justifyContent="flex-start"
       alignItems="center"
       flexDirection="column"
